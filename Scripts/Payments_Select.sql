e261dc8ec9afbb41895fce5620e4d72e7f5688a3
clairedane <68640542+clairedane@users.noreply.github.com>
2026-02-19 13:16:47 +0800

    Modify Payments_Select procedure to handle NULL reference

--- Scripts/Payments_Select.sql
+++ Scripts/Payments_Select.sql
@@ -1,8 +1,8 @@
 CREATE PROCEDURE [dbo].[Payments_Select]
-	@ReferenceID NVARCHAR(50)
+	@ReferenceID NVARCHAR(50) = NULL
 AS
 BEGIN
     SELECT * FROM Payments
-    WHERE ReferenceID = @ReferenceID
+    WHERE (@ReferenceID IS NULL OR @ReferenceID = '' OR ReferenceID = @ReferenceID)
 END
 GO
